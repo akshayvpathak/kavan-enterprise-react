1349bab24de6630fb1c56d643515256f45dba897
akshayp <akshayvpathak@yahoo.com>
2024-05-18 17:47:59 +0530

    changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -61,29 +61,29 @@ const Header = () => {
             <span className="relative block h-5.5 w-5.5 cursor-pointer">
               <span className="absolute right-0 block h-full w-full">
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                  className={`bg-linkhover relative left-0 top-0 my-1 block h-0.5 rounded-sm delay-[0] duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!w-full delay-300" : "w-0"
                   }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                  className={`bg-linkhover relative left-0 top-0 my-1 block h-0.5 rounded-sm delay-150 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "delay-400 !w-full" : "w-0"
                   }`}
                 ></span>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                  className={`bg-linkhover relative left-0 top-0 my-1 block h-0.5 rounded-sm delay-200 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!w-full delay-500" : "w-0"
                   }`}
                 ></span>
               </span>
               <span className="du-block absolute right-0 h-full w-full rotate-45">
                 <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
+                  className={`bg-linkhover absolute left-2.5 top-0 block h-full w-0.5 rounded-sm delay-300 duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!h-0 delay-[0]" : "h-full"
                   }`}
                 ></span>
                 <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
+                  className={`delay-400 bg-linkhover absolute left-0 top-2.5 block h-0.5 w-full rounded-sm duration-200 ease-in-out dark:bg-white ${
                     !navigationOpen ? "!h-0 delay-200" : "h-0.5"
                   }`}
                 ></span>
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
       <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
+            <div className=" sm:w-full md:w-1/2">
               <h1 className="text-linkhover mb-5 pr-16 text-3xl font-bold  xl:text-hero ">
                 Welcome to
                 <span className="text-linkhover relative  inline-block">
@@ -30,44 +30,13 @@ const Hero = () => {
               </p>
             </div>
 
-            <div className="animate_right  md:w-1/2 ">
-              <div className="relative 2xl:-mr-7.5">
-                <Image
-                  src="/images/shape/shape-01.png"
-                  alt="shape"
-                  width={46}
-                  height={246}
-                  className="absolute -left-11.5 top-0"
-                />
-                <Image
-                  src="/images/shape/shape-02.svg"
-                  alt="shape"
-                  width={36.9}
-                  height={36.7}
-                  className="absolute bottom-0 right-0 z-10"
-                />
-                <Image
-                  src="/images/shape/shape-03.svg"
-                  alt="shape"
-                  width={21.64}
-                  height={21.66}
-                  className="absolute -right-6.5 bottom-0 z-1"
-                />
-                <div className=" relative aspect-[700/444] w-full  bg-transparent">
-                  <Image
-                    className="shadow-solid-l dark:hidden"
-                    src="/images/custom/make-in-india.png"
-                    alt="Make In India"
-                    fill
-                  />
-                  <Image
-                    className="hidden shadow-solid-l dark:block"
-                    src="/images/custom/make-in-india.png"
-                    alt="Make In India"
-                    fill
-                  />
-                </div>
-              </div>
+            <div className="sm:w-full   md:w-1/2 ">
+              <Image
+                src="/images/custom/make-in-india.png"
+                alt="Make In India"
+                height={400}
+                width={400}
+              />
             </div>
           </div>
         </div>
