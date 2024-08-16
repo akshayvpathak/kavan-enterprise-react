2523cb7193a4a26e485dc7d7a5363bc3ec8ee129
akshayp <akshayvpathak@yahoo.com>
2024-08-16 19:20:03 +0530

    Update globe.tsx

--- components/ui/globe.tsx
+++ components/ui/globe.tsx
@@ -297,9 +297,9 @@ export function hexToRgb(hex: string) {
 }
 
 export function genRandomNumbers(min: number, max: number, count: number) {
-  const arr = [];
+  const arr: any = [];
   while (arr.length < count) {
-    const r = Math.floor(Math.random() * (max - min)) + min;
+    const r: any = Math.floor(Math.random() * (max - min)) + min;
     if (arr.indexOf(r) === -1) arr.push(r);
   }
 
